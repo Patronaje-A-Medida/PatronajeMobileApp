880d713e797ae902d7867bfc4ba5250c9a7c0ab2
Jeiel <jeiel.disas@gmail.com>
2022-04-03 11:28:14 -0500

    #14 modeloo para guia de toma de medidas corporales

---
+++ lib/domain/models/measures/measures_step.dart
@@ -0,0 +1,11 @@
+class MeasuresStep {
+  final String image;
+  final String title;
+  final String description;
+
+  MeasuresStep({
+    required this.image,
+    required this.title,
+    required this.description,
+  });
+}
