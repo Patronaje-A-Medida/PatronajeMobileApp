3ae5f402fe767c0cded876f84da59b58a70d92a0
Jeiel Tarazona Lovera <jeiel.disas@gmail.com>
2022-04-05 19:42:41 -0500

    #18 diseño y logica de la vista resultado-medidas

--- lib/business/measures/take_measures_provider.dart
+++ lib/business/measures/take_measures_provider.dart
@@ -3,13 +3,42 @@ import 'dart:io';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/services.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:patronaje_mobile_app/domain/models/measures/measures_temp.dart';
 
 class TakeMeasuresProvider extends ChangeNotifier {
-  File? imageFrontal;
-  File? imageLateral;
+  File? _imageFrontal;
+  File? _imageLateral;
+  bool _alreadyMeasures = false;
+
+  // placeholder
+  final List<BasicMeasure> basicMeasures = [
+    BasicMeasure(label: 'Altura', value: 160),
+    BasicMeasure(label: 'Busto', value: 80),
+    BasicMeasure(label: 'Cintura', value: 82.5),
+    BasicMeasure(label: 'Cadera', value: 90.7),
+  ];
+
+  final List<BasicMeasure> otherMeasures = [
+    BasicMeasure(label: 'Altura', value: 160),
+    BasicMeasure(label: 'Busto', value: 80),
+    BasicMeasure(label: 'Cintura', value: 82.5),
+    BasicMeasure(label: 'Cadera', value: 90.7),
+    BasicMeasure(label: 'Torax', value: 120),
+    BasicMeasure(label: 'Cadera alta', value: 92),
+    BasicMeasure(label: 'Muslo', value: 50.5),
+    BasicMeasure(label: 'Pantorillas', value: 27.8),
+    BasicMeasure(label: 'Entrepierna', value: 91.2),
+    BasicMeasure(label: 'Talle por delante', value: 65.2),
+    BasicMeasure(label: 'Talle por detrás', value: 73.5),
+    BasicMeasure(label: 'Hombros', value: 51.6),
+  ];
+
+  File? get imageFrontal => _imageFrontal;
+  File? get imageLateral => _imageLateral;
+  bool get alreadyMeasures => _alreadyMeasures;
 
   bool get canCalculateMeasures {
-    if (imageFrontal != null && imageLateral != null) {
+    if (_imageFrontal != null && _imageLateral != null) {
       notifyListeners();
       return true;
     } else {
@@ -20,11 +49,9 @@ class TakeMeasuresProvider extends ChangeNotifier {
 
   Future<void> takePhotoFrontal() async {
     try {
-      final photo = await ImagePicker().pickImage(source: ImageSource.camera);
-      if (photo == null) return;
-
-      final imageTemporary = File(photo.path);
-      this.imageFrontal = imageTemporary;
+      final imageTemporary = await takePhoto();
+      if (imageTemporary == null) return;
+      _imageFrontal = imageTemporary;
       notifyListeners();
     } on PlatformException catch (err) {
       print('Error al tomar la imagen ${err.toString()}');
@@ -33,14 +60,9 @@ class TakeMeasuresProvider extends ChangeNotifier {
 
   Future<void> takePhotoLateral() async {
     try {
-/*final photo = await ImagePicker().pickImage(source: ImageSource.camera);
-    if (photo == null) return;
-
-    final imageTemporary = File(photo.path);
-    this.imageLateral = imageTemporary;*/
       final imageTemporary = await takePhoto();
       if (imageTemporary == null) return;
-      this.imageLateral = imageTemporary;
+      _imageLateral = imageTemporary;
       notifyListeners();
     } on PlatformException catch (err) {
       print('Error al tomar la imagen ${err.toString()}');
@@ -51,5 +73,19 @@ class TakeMeasuresProvider extends ChangeNotifier {
     final photo = await ImagePicker().pickImage(source: ImageSource.camera);
     if (photo == null) return null;
     return File(photo.path);
+  }
+
+  Future<void> calculateMeasures() async {
+    _alreadyMeasures = false;
+    notifyListeners();
+    await Future.delayed(const Duration(seconds: 5), () {
+      _alreadyMeasures = true;
+      notifyListeners();
+    });
+  }
+
+  void resetPhotos() {
+    _imageFrontal = null;
+    _imageLateral = null;
   }
 }
---
+++ lib/presentation/measures/measures_results_page.dart
@@ -0,0 +1,121 @@
+import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+import 'package:patronaje_mobile_app/business/measures/take_measures_provider.dart';
+import 'package:patronaje_mobile_app/domain/utils/themes/color_theme.dart';
+import 'package:provider/provider.dart';
+
+class MeasuresResultsPage extends StatelessWidget {
+  const MeasuresResultsPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final takeMeasuresProvider = Provider.of<TakeMeasuresProvider>(context);
+    final basicMeasures = takeMeasuresProvider.basicMeasures;
+    final otherMeasures = takeMeasuresProvider.otherMeasures;
+    final sizeH = MediaQuery.of(context).size.height;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Resultados'),
+        leading: IconButton(
+          icon: const Icon(
+            Icons.arrow_back_ios,
+            size: 18,
+          ),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+      ),
+      body: !takeMeasuresProvider.alreadyMeasures
+          ? Center(
+              child: Column(
+                children: [
+                  Lottie.asset('assets/animations/calculate_loading.json'),
+                  const Text('Calculando . . .',
+                      style:
+                          TextStyle(fontSize: 18, fontWeight: FontWeight.w600)),
+                ],
+              ),
+            )
+          : Padding(
+              padding: const EdgeInsets.all(18.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  Center(
+                    child: Image.asset(
+                      'assets/images/basic_measures.png',
+                      height: sizeH * 0.4,
+                    ),
+                  ),
+                  const Padding(
+                    padding: EdgeInsets.symmetric(vertical: 12.0),
+                    child: Text('Medidas en centímetros'),
+                  ),
+                  Row(
+                    children: List.generate(basicMeasures.length, (index) {
+                      return Expanded(
+                        child: _buildBasicMeasure(
+                          measureLabel: basicMeasures[index].label,
+                          measureSize: basicMeasures[index].value,
+                          number: index + 1,
+                        ),
+                      );
+                    }),
+                  ),
+                  const SizedBox(height: 12),
+                  Expanded(
+                    child: ListView(children: [
+                      DataTable(
+                        columns: const [
+                          DataColumn(label: Text('Sección')),
+                          DataColumn(label: Text('Medida')),
+                        ],
+                        rows: otherMeasures.map((e) {
+                          return DataRow(cells: [
+                            DataCell(Text(e.label)),
+                            DataCell(Text(e.value.toString())),
+                          ]);
+                        }).toList(),
+                      ),
+                    ]),
+                  ),
+                ],
+              ),
+            ),
+    );
+  }
+
+  Widget _buildBasicMeasure(
+      {required String measureLabel,
+      required double measureSize,
+      required int number}) {
+    return Column(
+      children: [
+        Text(
+          measureLabel,
+          style: const TextStyle(
+            fontWeight: FontWeight.w600,
+            color: Palette.pink,
+            fontSize: 16,
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(vertical: 4.0),
+          child: CircleAvatar(
+            child: Text(
+              number.toString(),
+              style: const TextStyle(
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            backgroundColor: Palette.pink,
+            foregroundColor: Colors.white,
+          ),
+        ),
+        Text(measureSize.toString()),
+      ],
+    );
+  }
+}
