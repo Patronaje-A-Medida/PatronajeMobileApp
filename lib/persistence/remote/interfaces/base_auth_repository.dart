3f18ca6ace3e75b5842479303d2e0e8513716581
Jeiel Tarazona Lovera <jeiel.disas@gmail.com>
2022-04-06 18:59:57 -0500

    #22 agregado funcion para actualizar perfin el auth-repository

--- lib/persistence/remote/interfaces/base_auth_repository.dart
+++ lib/persistence/remote/interfaces/base_auth_repository.dart
@@ -1,8 +1,11 @@
 import 'package:patronaje_mobile_app/domain/models/auth/user_client_token.dart';
+import 'package:patronaje_mobile_app/domain/models/auth/user_client_update.dart';
 import 'package:patronaje_mobile_app/domain/models/auth/user_create.dart';
 import 'package:patronaje_mobile_app/domain/models/auth/user_login.dart';
+import 'package:patronaje_mobile_app/domain/models/auth/user_read.dart';
 
 abstract class BaseAuthRepository {
   Future<UserClientToken> signUp(UserCreate userCreate);
   Future<UserClientToken> signIn(UserLogin userLogin);
+  Future<UserRead> updateProfile(UserClientUpdate userClientUpdate);
 }
