379ca9a160cb8e980213744545b7ef41c1d6acd3
Jeiel Tarazona Lovera <jeiel.disas@gmail.com>
2022-04-07 18:23:41 -0500

    #26 creacion de create order en order-repiository

--- lib/persistence/remote/interfaces/base_order_repository.dart
+++ lib/persistence/remote/interfaces/base_order_repository.dart
@@ -1,5 +1,7 @@
 import 'package:patronaje_mobile_app/domain/models/garments/order.dart';
+import 'package:patronaje_mobile_app/domain/models/orders/order_create.dart';
 
 abstract class BaseOrderRepository {
   Future<List<Order>> getOrdersByClientId();
+  Future<bool> createOrder(OrderCreate orderCreate);
 }
